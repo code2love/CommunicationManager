102ccc7dc6b52b18f5293861d830846088643d02
Daniel Stuemke <dastit03@hs-esslingen.de>
2019-03-02 19:21:55 +0100

    Bugfix in Update Method

--- CommunicationManager.h
+++ CommunicationManager.h
@@ -5,110 +5,119 @@
  * Date:    19.02.2019
  *
  */
-#ifndef __COMMUNICATION_MANAGER_H__
-#define __COMMUNICATION_MANAGER_H__
+ #ifndef __COMMUNICATION_MANAGER_H__
+ #define __COMMUNICATION_MANAGER_H__
 
-#include "FlexCAN.h"
+ #ifndef COMMUNICATION_TEST_ENV
+ #include "FlexCAN.h"
+ #endif
+
+  /************************************************************************
+   * Debug Macros and definitions
+   */
+
+ #define COMMUNICATION_DEBUG_MODE 1
+
+ #define COMMUNICATION_DEBUG_PRINT(...) if(1 == COMMUNICATION_DEBUG_MODE) Serial.print(__VA_ARGS__)
+ #define COMMUNICATION_DEBUG_PRINTLN(...) if(1 == COMMUNICATION_DEBUG_MODE) Serial.println(__VA_ARGS__)
 
 
-/************************************************************************
- * Debug Macros and definitions
- */
-#define COMMUNICATION_DEBUG_MODE 1
+ enum COMMUNICATION_CYCLE { CYCLE_10 = 0, CYCLE_20, CYCLE_40, CYCLE_80, CYCLE_100 };
 
-#define COMMUNICATION_DEBUG_PRINT(...) if(1 == COMMUNICATION_DEBUG_MODE) Serial.print(__VA_ARGS__)
-#define COMMUNICATION_DEBUG_PRINTLN(...) if(1 == COMMUNICATION_DEBUG_MODE) Serial.println(__VA_ARGS__)
+ typedef struct COMMUNICATION_producer_t {
+ 	unsigned char* ref;
+ 	unsigned char bytes;
+ 	unsigned int canId;
+ 	unsigned char* txFlag;
+ 	COMMUNICATION_CYCLE cycle;
+ } COMMUNICATION_producer_t;
+
+ typedef struct COMMUNICATION_consumer_t {
+ 	unsigned char* ref;
+ 	unsigned char bytes;
+ 	unsigned int canId;
+ 	unsigned char* rxFlag;
+ } COMMUNICATION_consumer_t;
+
+ typedef struct COMMUNICATION_listNode_t {
+ 	COMMUNICATION_producer_t producer;
+ 	COMMUNICATION_listNode_t* left;
+ 	COMMUNICATION_listNode_t* right;
+ } COMMUNICATION_listNode_t;
+
+ #define COMMUNICATION_MAX_CONSUMERS 128
+ #define COMMUNICATION_MAX_PRODUCERS 128
+ #define COMMUNICATION_FIRE_STACK_SIZE 8
+ #define COMMUNICATION_MAX_LIST_NODES 96
 
 
-enum COMMUNICATION_CYCLE { CYCLE_10 = 0, CYCLE_20, CYCLE_40, CYCLE_80, CYCLE_100 };
+ enum COMMUNICATION_BYTE_ORDER { ORDER_MSB, ORDER_LSB };
 
-typedef struct COMMUNICATION_producer_t {
-  unsigned char* ref;
-  unsigned char bytes;
-  unsigned int canId;
-  unsigned char* txFlag;
-  COMMUNICATION_CYCLE cycle;
-} COMMUNICATION_producer_t;
+ class CommunicationManager {
+ private:
+ 	CommunicationManager();
 
-typedef struct COMMUNICATION_consumer_t {
-  unsigned char* ref;
-  unsigned char bytes;
-  unsigned int canId;
-  unsigned char* rxFlag;
-} COMMUNICATION_consumer_t;
+ #ifndef COMMUNICATION_TEST_ENV
+ 	CAN_message_t CAN_outMsg;
+ 	CAN_message_t CAN_inMsg;
 
-typedef struct COMMUNICATION_listNode_t {
-  COMMUNICATION_producer_t producer;
-  COMMUNICATION_listNode_t* left;
-  COMMUNICATION_listNode_t* right;
-} COMMUNICATION_listNode_t;
+ 	FlexCAN CAN_Can0;
+ 	CAN_filter_t CAN_filter;
+ #endif
 
-#define COMMUNICATION_MAX_CONSUMERS 128
-#define COMMUNICATION_MAX_PRODUCERS 128
-#define COMMUNICATION_FIRE_STACK_SIZE 8
-#define COMMUNICATION_MAX_LIST_NODES 96
+ 	void InitCan(uint32_t baud);
+ 	int SendCanMessage(uint16_t msgID, uint8_t *data, uint8_t lengthOfData);
+ 	int ReceiveCanMessage(uint32_t *id, uint8_t *buf, int n);
 
-class CommunicationManager {
-  private:
-  CommunicationManager();
+ 	COMMUNICATION_listNode_t nodes[COMMUNICATION_MAX_LIST_NODES];
+ 	COMMUNICATION_listNode_t* freeNodes[COMMUNICATION_MAX_LIST_NODES];
 
-  CAN_message_t CAN_outMsg;
-  CAN_message_t CAN_inMsg;
+ 	unsigned int nNodes;
+ 	unsigned int maxNodesUsed;
 
-  FlexCAN CAN_Can0;
-  CAN_filter_t CAN_filter;
+ 	void InitNodes();
+ 	COMMUNICATION_listNode_t* NewNode();
+ 	void DeleteNode(COMMUNICATION_listNode_t* node);
 
-  void InitCan(uint32_t baud);
-  int SendCanMessage(uint16_t msgID, uint8_t *data, uint8_t lengthOfData);
-  int ReceiveCanMessage(uint32_t *id, uint8_t *buf, int n);
+ 	COMMUNICATION_listNode_t* nodeList;
 
-  COMMUNICATION_listNode_t nodes[COMMUNICATION_MAX_LIST_NODES];
-  COMMUNICATION_listNode_t* freeNodes[COMMUNICATION_MAX_LIST_NODES];
+ 	void InitList();
+ 	bool ListAdd(COMMUNICATION_producer_t producer);
+ 	void ListAdd(COMMUNICATION_listNode_t* node);
+ 	COMMUNICATION_producer_t ListGetHead();
+ 	void ListRemoveHead();
+ 	bool ListEmpty();
 
-  unsigned int nNodes;
-  unsigned int maxNodesUsed;
+ 	COMMUNICATION_producer_t producers[COMMUNICATION_MAX_PRODUCERS];
+ 	COMMUNICATION_consumer_t consumers[COMMUNICATION_MAX_CONSUMERS];
 
-  void InitNodes();
-  COMMUNICATION_listNode_t* NewNode();
-  void DeleteNode(COMMUNICATION_listNode_t* node);
+ 	unsigned int nProducers;
+ 	unsigned int nConsumers;
 
-  COMMUNICATION_listNode_t* nodeList;
+ 	COMMUNICATION_producer_t emergencies[COMMUNICATION_FIRE_STACK_SIZE];
 
-  void InitList();
-  bool ListAdd(COMMUNICATION_producer_t producer);
-  void ListAdd(COMMUNICATION_listNode_t* node);
-  COMMUNICATION_producer_t ListGetHead();
-  void ListRemoveHead();
-  bool ListEmpty();
+ 	unsigned int nEmergencies;
 
-  COMMUNICATION_producer_t producers[COMMUNICATION_MAX_PRODUCERS];
-  COMMUNICATION_consumer_t consumers[COMMUNICATION_MAX_CONSUMERS];
+ 	COMMUNICATION_BYTE_ORDER byteOrder;
 
-  unsigned int nProducers;
-  unsigned int nConsumers;
+ public:
+ 	static CommunicationManager* GetInstance();
 
-  COMMUNICATION_producer_t emergencies[COMMUNICATION_FIRE_STACK_SIZE];
+ 	void Initialize(uint32_t baud = 500000, COMMUNICATION_BYTE_ORDER byteOrder = ORDER_MSB);
 
-  unsigned int nEmergencies;
+ 	unsigned int GetMessageUtilization();
 
-  public:
-  static CommunicationManager* GetInstance();
+ 	unsigned int GetMaxMessageUtilization();
 
-  void Initialize(uint32_t baud = 500000);
+ 	bool Fire(unsigned int canId);
 
-  unsigned int GetMessageUtilization();
+ 	bool Fire(void* val, unsigned int bytes, unsigned int canId);
 
-  unsigned int GetMaxMessageUtilization();
+ 	bool Publish(void* val, unsigned int bytes, unsigned int canId, unsigned char* txFlag, COMMUNICATION_CYCLE cycle);
 
-  bool Fire(unsigned int canId);
+ 	bool Subscribe(void* val, unsigned int bytes, unsigned int canId, unsigned char* rxFlag);
 
-  bool Fire(void* val, unsigned int bytes, unsigned int canId);
+ 	void Update();
+ };
 
-  bool Publish(void* val, unsigned int bytes, unsigned int canId, unsigned char* txFlag, COMMUNICATION_CYCLE cycle);
-
-  bool Subscribe(void* val, unsigned int bytes, unsigned int canId, unsigned char* rxFlag);
-
-  void Update();
-};
-
-#endif
+ #endif
